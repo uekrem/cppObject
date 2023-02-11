4e18dcfe1eae122ef4d41f05241d79479c6e9696
H. Utku Ekrem <110349452+uekrem@users.noreply.github.com>
2023-02-11 19:45:25 +0300

    Add files via upload

---
+++ sil.cpp
@@ -0,0 +1,37 @@
+#include "sil.h"
+using namespace std;
+
+class student
+{
+    public:
+        string  name;
+        int     no;
+        void    getname()
+        {
+            cout<< "Nesnemizin Adı:" << name << endl;
+        }
+};
+
+int main()
+{
+    int now_year;
+    student     std1;
+    student2    std2;
+    car         car1;
+
+    std1.name = "Harun Utku Ekrem";
+    std1.no = 123;
+    std1.getname();
+
+    std2.name = "Mehmet";
+    std2.no = 234;
+    std2.getno();
+
+    now_year = 2023;
+    car1.setmodel("Skoda");
+    car1.setyear(2016);
+    car1.setage(now_year);
+    car1.getmodel();
+    car1.getyear();
+    car1.getage();
+}
---
+++ sil.h
@@ -0,0 +1,36 @@
+#ifndef SIL_H
+# define SIL_H
+
+#include <iostream>
+using namespace std;
+
+class student2
+{
+    public:
+        string  name;
+        int     no;
+        void    getno();
+};
+
+class car
+{
+    private:
+        string  model;
+        int     year;
+        int     age;
+    public:
+        void    setmodel(string input)
+        {
+            model = input;
+        }
+        void    getmodel()
+        {
+            cout << "Nesne Modeli:" << model << endl;
+        }
+        void    setyear(int input);
+        void    getyear();
+        void    setage(int  now_time);
+        void    getage();
+};
+
+#endif
