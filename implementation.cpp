97b7a3d8531929d845fb6df7eea3a021b052b4ed
H. Utku Ekrem <110349452+uekrem@users.noreply.github.com>
2023-02-11 19:46:03 +0300

    Add files via upload

---
+++ implementation.cpp
@@ -0,0 +1,28 @@
+#include "sil.h"
+
+            //CLASS STUUDENT2
+void    student2::getno()
+{
+    cout << "Nesnenin No:" << student2::no << endl;
+}
+
+            //CLASS CAR
+void    car::setyear(int input)
+{
+    car::year = input;
+}
+
+void    car::getyear()
+{
+    cout << "Yılı:" << car::year << endl;
+}
+
+void    car::setage(int now_time)
+{
+    car::age = now_time - car::year;
+}
+
+void    car::getage()
+{
+    cout << "Yasi:" << car::age << endl;
+}
