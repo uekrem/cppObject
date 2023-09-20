e2dc751cedb41831162fefcb7d0616d86c27f5b7
Harun utku Ekrem <hekrem@k1m35s02.42istanbul.com.tr>
2023-09-20 11:37:02 +0300

    20_09

--- NOTED/03/step3/main.cpp
+++ NOTED/03/step3/main.cpp
@@ -13,6 +13,50 @@ int main()
             //cagrilmali
     */
 
+    /*
+    Base2   *ptr;
+    Old2    o2;
+    ptr = &o2;
+    ptr.ft_screw(); (FALSE)
+    //Miras aldığı class türüne atandığı zaman mirasçısından aldığı
+        //metotları kullanabilir . Kendi yapısının içerisinde ki
+            //metot erişilemez.
+    */
+
+    /*
+    Old3    r;
+    Base3   ptr;
+    //Burada pointersız atama işlemi yapıldığında içerisindeki değerler değiştirilir
+        //O yuzden dolayı kayıp olmaz ve polymorp yaşanmaz .
+    r.c = 5;
+    ptr = r; 
+    ptr.ft_print();
+    ptr.ft_show();
+    */
+
+    /*
+    Old3    r(3);
+    Old3    rr;
+    rr = r; 
+    std::cout << rr.base_int << std::endl;
+    std::cout << rr.old_int << std::endl;
+    std::cout << r.base_int << std::endl;
+    std::cout << r.old_int << std::endl;
+    //overloading işlem uygulandıysa miras aldığımız değerleri artı bir
+        //şekilde çağırmamız gerekir . Yoksa miras aldığımız class değerlerini
+            // birbirine atayamayız.
+    */
+
+    /*
+    Old3    r;
+    Base3   *ptr;
+    //Pointer kullanıldığı zaman adresini işaret ettiği için virtual kullanıl-
+        //masi gerekir . Miras alınan class içerisinde ki metot ezilip kendi
+            //classımız içerisinde ki metodu kullanabilelim .
+    ptr = &r; 
+    ptr->ft_print();
+    ptr->ft_show();
+    */
 
     
 }
--- NOTED/03/step3/main.hpp
+++ NOTED/03/step3/main.hpp
@@ -24,6 +24,57 @@ class Old : public Base{
 };
 
 
+class Base2{
+    private:
+    public:
+        void    ft_rew(){
+            std::cout << "ft_rew" << std::endl;   
+        }
+};
+class Old2 : public Base2{
+    private:
+    public:
+        void    ft_screw(){
+            std::cout << "ft_screw" << std::endl;   
+        }
+};
 
+
+class Base3{
+    private:
+    public:
+        int base_int;
+        Base3(){
+            base_int = 10;
+        }
+        virtual void    ft_print(){
+            std::cout << "Base3 ft_print" << std::endl;
+        }
+        void    ft_show(){
+            std::cout << "Base3 ft_show" << std::endl;
+        }
+};
+class Old3 : public Base3{
+    private:
+    public:
+        int old_int;
+        Old3(){
+            this->base_int = -1;
+            this->old_int = -1;
+        }
+        Old3(int a){
+            this->base_int = a;
+            this->old_int = a;
+        }
+        void    ft_print(){
+            std::cout << "Old3 ft_print" << std::endl;
+        }
+        void    ft_show(){
+            std::cout << "Old3 ft_show" << std::endl;
+        }
+        void    operator=(const Old3 &ref){
+            this->old_int = ref.old_int;
+        }
+};
 
 #endif
