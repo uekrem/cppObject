ea7d084a24e5d3954c5b7e7a58d53e9d2b8f2371
Harun utku Ekrem <hekrem@k1m38s01.42istanbul.com.tr>
2023-09-23 12:46:10 +0300

    23_09

--- NOTED/03/step4/main.cpp
+++ NOTED/03/step4/main.cpp
@@ -2,5 +2,5 @@
 
 int main()
 {
-    
+    nwd n1;
 }
--- NOTED/03/step4/main.hpp
+++ NOTED/03/step4/main.hpp
@@ -3,6 +3,32 @@
 
 #include <iostream>
 
+class Base{
+    public:
+    Base(){
+        std::cout << "Base cons" << std::endl;
+    }
+};
 
+class Old1:virtual public Base{
+    public:
+    Old1(){
+        std::cout << "Old1 cons" << std::endl;
+    }
+};
+
+class Old2:virtual public Base{
+    public:
+    Old2(){
+        std::cout << "Old2 cons" << std::endl;
+    }
+};
+
+class nwd:public Old2, public Old1{
+    public:
+    nwd(){
+        std::cout << "nwd cons" << std::endl;
+    }
+};
 
 #endif
